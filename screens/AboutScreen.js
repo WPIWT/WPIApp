a21398ab4412c505825ae1e6c0f79e5f4bd021b0
Manjusha Chava <manjushachava@gmail.com>
2019-07-17 17:06:52 -0400

    Changed About page to look pretty.

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -3,15 +3,19 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable global-require */
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet, Text, View, Image,
+} from 'react-native';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { ScrollView } from 'react-native-gesture-handler';
+import { Font, AppLoading } from 'expo';
+import { Root } from 'native-base';
+import { Divider } from 'react-native-elements';
 
 const styles = StyleSheet.create({
   titleText: {
-    fontFamily: 'Optima',
-    fontSize: 36,
-    fontWeight: 'bold',
+    fontFamily: 'MyriadPro-Bold',
+    fontSize: 24,
   },
   header: {
     marginTop: 35,
@@ -22,20 +26,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  image: {
+    alignItems: 'center',
+  },
   divContainer: {
     marginTop: 0,
-    height: 100,
+    height: 70,
     fontSize: 20,
     backgroundColor: '#E8E8E8',
   },
   paraText: {
-    fontSize: 20,
-    fontFamily: 'Optima',
+    fontSize: 16,
+    fontFamily: 'MyriadPro-Light',
   },
   paragraph: {
-    marginTop: 50,
+    flex: 1,
+    marginTop: 30,
     marginLeft: 20,
     marginRight: 20,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paragraph1: {
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
@@ -51,27 +64,52 @@ class AboutScreen extends React.Component {
     headerTintColor: 'white',
   }
 
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'MinionPro-BoldDisp': require('../assets/fonts/MinionPro-BoldDisp.ttf'),
+      'MyriadPro-Light': require('../assets/fonts/MyriadPro-Light.ttf'),
+      'MyriadPro-Regular': require('../assets/fonts/MyriadPro-Regular.ttf'),
+      'MyriadPro-Bold': require('../assets/fonts/MyriadPro-Bold.ttf'),
+      'SpaceMono-Regular': require('../assets/fonts/SpaceMono-Regular.ttf'),
+    });
+    this.setState({ loading: false });
+  }
+
   render() {
+    if (this.state.loading) {
+      return (
+        <Root>
+          <AppLoading />
+        </Root>
+      );
+    }
     return (
       <View style={styles.container}>
-        <View style={styles.divContainer}>
-          <View style={styles.header}>
-            <Text style={styles.titleText}>About</Text>
+        <ScrollView>
+          <View style={styles.image}>
+            <Image
+              style={{ marginTop: 20, width: 150, height: 150 }}
+              source={require('../assets/images/WPI_logo.png')}
+            />
           </View>
-        </View>
-        <View style={styles.paragraph}>
-          <ScrollView>
+          <View style={styles.paragraph}>
             <Text style={styles.paraText}>
-              {' '}
               The idea for this app was conceived by Sydney Messey, Dennis Leary, and Anika Stundtner
-              through the Stanford University Innovation Fellowship as a means to unite resources in a
-              centralized accessible hub for the WPI community.
+              through the Stanford University Innovation Fellowship.
+            </Text>
+            <Divider style={{ backgroundColor: 'white', marginVertical: 10 }} />
+            <Text style={styles.paraText}>
               This app was developed by Manjusha Chava, Elaine Chen, Isabelle Cordova, and Lucas Varella
               with the help of WPI Web Tech executives John Dyer, Chris Mercer, and Jake Scheide.
               Funding for the development of this app was provided by the KEEN Grant.
             </Text>
-          </ScrollView>
-        </View>
+          </View>
+        </ScrollView>
       </View>
     );
   }
