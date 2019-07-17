0a293a65ee71557ed43ed645a30a8b152fdbf9d9
Manjusha Chava <manjushachava@gmail.com>
2019-07-17 16:04:18 -0400

    Added a nav bar with name and back arrow. Scru dis

--- screens/SecondLevel/LaundryLinks.js
+++ screens/SecondLevel/LaundryLinks.js
@@ -10,34 +10,70 @@ import {
 import { MaterialIcons } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
-  MainContainer: {
+  navBar: {
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#AC2B37',
+    marginTop: 10,
+  },
+  leftContainer: {
     flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
     backgroundColor: '#AC2B37',
   },
+  rightContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: '#AC2B37',
+  },
+  rightIcon: {
+    height: 10,
+    width: 10,
+    resizeMode: 'contain',
+    backgroundColor: '#AC2B37',
+  },
+  MainContainer: {
+    backgroundColor: '#AC2B37',
+    flex: 1,
+  },
   TextStyle: {
-    marginTop: 25,
-    textAlign: 'center',
     fontSize: 16,
     fontFamily: 'Helvetica-Bold',
     color: '#fff',
   },
   icon: {
     textAlign: 'left',
+    justifyContent: 'space-evenly',
+    alignItems: 'flex-start',
+    marginTop: 10,
   },
 });
-
 
 export default class LaundryLinks extends Component {
   render() {
     return (
+
       <View style={styles.MainContainer}>
-        <Text style={styles.TextStyle}>
-          {this.props.navigation.getParam('name', 'Laundry Links')}
-          <MaterialIcons name="chevron-left" size={42} color="white" style={styles.icon} onPress={() => this.props.navigation.navigate('LaundryScreen')} />
-        </Text>
+        <View style={styles.navBar}>
+          <View style={styles.leftContainer}>
+            <Text style={[styles.text, { textAlign: 'left' }]}>
+              <MaterialIcons name="chevron-left" size={42} color="white" style={styles.icon} onPress={() => this.props.navigation.navigate('LaundryScreen')} />
+            </Text>
+          </View>
+          <Text style={styles.TextStyle}>
+            {this.props.navigation.getParam('name', 'Laundry Links')}
+          </Text>
+          <View style={styles.rightContainer}>
+            <View style={styles.rightIcon} />
+          </View>
+        </View>
         <WebView
           source={{ uri: this.props.navigation.getParam('link', 'https://www.google.com') }}
-          style={{ marginTop: 20 }}
         />
       </View>
     );
